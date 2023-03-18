93e33fd253440b454976fb9a52174ca3f6dc045b
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-03-18 23:54:00 +0200

    implementing sign up functionality and creating JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,22 @@
 const User = require('./../models/usersmodel');
 const catchAsyncError = require('./../utils/catchAsyncError');
+const JWT = require('jsonwebtoken');
 
 exports.signup = catchAsyncError(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  const token = JWT.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
