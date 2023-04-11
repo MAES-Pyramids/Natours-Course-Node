ba5b1a54cb723794a54206d8d9f8119af2b05c9e
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-04-11 20:52:33 +0200

    sending JWT as cookies not only response header

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 const { promisify } = require('util');
-const User = require('./../models/usersmodel');
 const crypto = require('crypto');
+const JWT = require('jsonwebtoken');
 const catchAsyncError = require('./../utils/catchAsyncError');
 const AppError = require('./../utils/appError');
 const sendEmail = require('./../utils/email');
-const JWT = require('jsonwebtoken');
+const User = require('./../models/usersmodel');
 //-----------------JWT-----------------------//
 function signToken(id) {
   return JWT.sign({ id: id }, process.env.JWT_SECRET, {
@@ -13,6 +13,23 @@ function signToken(id) {
 }
 function createAndSendToken(user, statusCode, res) {
   const token = signToken(user._id);
+
+  // Set cookie options
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // remove password from  user data output
+  user.password = undefined;
+
+  // Send cookie
+  res.cookie('jwt', token, cookieOptions);
+
+  // Send response
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -36,22 +53,17 @@ exports.signup = catchAsyncError(async (req, res, next) => {
 // Login
 exports.login = catchAsyncError(async (req, res, next) => {
   const { email, password } = req.body;
-  const login_Password = password;
   // 1) Check if email and password exist
-  if (!email && !login_Password)
+  if (!email && !password)
     return next(new AppError('Please provide email and password', 400));
   if (!email) return next(new AppError('Please provide email', 400));
-  if (!login_Password)
-    return next(new AppError('Please provide password', 400));
+  if (!password) return next(new AppError('Please provide password', 400));
 
   // 2) Check if user exists && password is correct
   const user = await User.findOne({ email }).select('+password');
-  const correctPass = await user?.correctPassword(
-    login_Password,
-    user.password
-  );
-  if (!user || !correctPass)
+  if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401));
+  }
 
   // 3) If everything ok, send token to client
   createAndSendToken(user, 200, res);
