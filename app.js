66cd80324f0c7e4f5600c04e4e09ed4c46f6cde5
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-04-12 15:10:19 +0200

    impelementing rate limiting using express-rate-limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const morgan = require('morgan');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorsController');
@@ -8,16 +9,27 @@ const toursRouter = require('./routes/toursRoutes');
 const usersRouter = require('./routes/usersRoutes');
 //-------------------------------------------//
 const app = express();
-//---------------middleware------------------//
+//------------Global middleware--------------//
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Add a timestamp to each request
 app.use((req, res, next) => {
   req.time = new Date().toISOString();
   next();
 });
 
+// Serve static content located in the "public" directory.
 app.use(express.static('public'));
+
+// Implement a simple rate limiting
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+//--------------Global Routing--------------//
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
