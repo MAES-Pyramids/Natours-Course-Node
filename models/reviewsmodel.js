a0294caccac93b898c7540b1d1be07ee5cd80be0
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-04-16 00:16:39 +0200

    Average Rating on Tours mongoose static method to impalement aggregation pipeline

--- models/reviewsmodel.js
+++ models/reviewsmodel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./toursmodel');
 //-------------------Schema----------------//
 const reviewsSchema = new mongoose.Schema({
   review: {
@@ -41,6 +42,35 @@ reviewsSchema.pre(/^find/, function(next) {
     select: 'name photo -_id'
   });
   next();
+});
+//--------------------Static-----------------//
+reviewsSchema.statics.calcAverageRatings = async function(tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: stats[0].nRating,
+      ratingAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+//--------------------Document-----------------//
+reviewsSchema.post('save', function() {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
 });
 //--------------------Model------------------//
 const Review = mongoose.model('Review', reviewsSchema);
