4076334e511ef2c62746a091e1d564f59b62d4a2
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-04-28 02:09:30 +0300

    Uploading Multiple Images

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -2,6 +2,8 @@ const catchAsyncError = require('./../utils/catchAsyncError');
 const AppError = require('./../utils/appError');
 const Tour = require('./../models/toursmodel');
 const factory = require('./handlerFactory');
+const multer = require('multer');
+const sharp = require('sharp');
 //--------------Alias Methods----------------//
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -15,6 +17,54 @@ exports.getAllTours = factory.getAll(Tour);
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
+//---------------------Upload Photos--------------------//
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 }
+]);
+exports.resizeTourImages = catchAsyncError(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+});
 //-----------------aggregation pipeline-----------------//
 //--method 1--//
 exports.getTourStats = catchAsyncError(async (req, res, next) => {
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -41687,12 +41687,11 @@ if (logoutBtn) {
 if (updateSettingsBtn) {
   updateSettingsBtn.addEventListener('submit', function (event) {
     event.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    (0, _updateSettings.updatesettings)({
-      name: name,
-      email: email
-    }, 'data');
+    var form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    (0, _updateSettings.updatesettings)(form, 'data');
   });
 }
 if (updatePasswordBtn) {
@@ -41734,7 +41733,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51812" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55770" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
