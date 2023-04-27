ed34007bb9a88decfc987b3e576fe82fb637825f
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-04-28 02:55:01 +0300

    refactoring tours routes

--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -3,34 +3,12 @@ const toursController = require('./../controllers/toursController');
 const reviewsRouter = require('./reviewsRoutes');
 const authController = require('./../controllers/authController');
 
-//-------------------------------------------//
 const router = express.Router();
-//------------------ROUTES-------------------//
+
+// Reviews Routes
 router.use('/:tourID/reviews', reviewsRouter);
-router.get('/tour-stats', toursController.getTourStats);
-router.get(
-  '/top_5_rated',
-  toursController.aliasTopTours,
-  toursController.getAllTours
-);
-router.get(
-  '/monthly-plan/:year',
-  authController.protect,
-  authController.restrictTo('admin', 'lead-guide', 'guide'),
-  toursController.getMonthlyPlan
-);
-//--------------Geospatial Routes-------------//
-router.get(
-  '/tours-within/:distance/center/:latlng/unit/:unit',
-  authController.protect,
-  toursController.getToursWithin
-);
-router.get(
-  '/distances/:latlng/unit/:unit',
-  authController.protect,
-  toursController.getDistances
-);
-// ------------------------------------------//
+
+// Tours Routes
 router
   .route('/')
   .get(toursController.getAllTours)
@@ -46,6 +24,8 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    toursController.uploadTourImages,
+    toursController.resizeTourImages,
     toursController.updateTour
   )
   .delete(
@@ -54,6 +34,32 @@ router
     toursController.deleteTour
   );
 
+// Tours Statistics and Aggregation Routes
+router.get('/tour-stats', toursController.getTourStats);
+router.get(
+  '/top_5_rated',
+  toursController.aliasTopTours,
+  toursController.getAllTours
+);
+router.get(
+  '/monthly-plan/:year',
+  authController.protect,
+  authController.restrictTo('admin', 'lead-guide', 'guide'),
+  toursController.getMonthlyPlan
+);
+
+// Geospatial Routes
+router.get(
+  '/tours-within/:distance/center/:latlng/unit/:unit',
+  authController.protect,
+  toursController.getToursWithin
+);
+router.get(
+  '/distances/:latlng/unit/:unit',
+  authController.protect,
+  toursController.getDistances
+);
+
 // router
 //   .route('/:tourID/review')
 //   .post(
@@ -61,5 +67,5 @@ router
 //     authController.restrictTo('user'),
 //     reviewsController.createReview
 //   );
-//-------------------------------------------//
+
 module.exports = router;
