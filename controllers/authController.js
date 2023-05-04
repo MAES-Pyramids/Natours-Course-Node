68b66cdebc6339936f865179a7c22efe4559c6bb
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-05-04 10:38:15 +0300

    update Authentication with complex email handling class

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
-const { promisify } = require('util');
 const crypto = require('crypto');
 const JWT = require('jsonwebtoken');
-const catchAsyncError = require('./../utils/catchAsyncError');
-const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const { promisify } = require('util');
+const Email = require('./../utils/email');
 const User = require('./../models/usersmodel');
+const AppError = require('./../utils/appError');
+const catchAsyncError = require('./../utils/catchAsyncError');
 //-----------------JWT-----------------------//
 function signToken(id) {
   return JWT.sign({ id: id }, process.env.JWT_SECRET, {
@@ -48,6 +48,9 @@ exports.signup = catchAsyncError(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
     role: req.body.role
   });
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
+
   createAndSendToken(newUser, 201, res);
 });
 //-------------- Login
@@ -135,18 +138,13 @@ exports.forgotPassword = catchAsyncError(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
-
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message
-    });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    await new Email(user, resetURL).sendPasswordReset();
+
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!'
