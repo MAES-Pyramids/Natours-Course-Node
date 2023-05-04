7bb9081870fad74167e8be7345c2d5c0e07b1598
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-05-04 17:41:25 +0300

    prepare for deployment and integrate stripe for emails

--- models/usersmodel.js
+++ models/usersmodel.js
@@ -83,12 +83,6 @@ userSchema.methods.createPasswordResetToken = function() {
     .update(resetToken)
     .digest('hex');
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
-  console.log(
-    { resetToken },
-    this.passwordResetToken,
-    `Expires: ${this.passwordResetExpires}`
-  );
   return resetToken;
 };
 //-------------------Managing Password------------------//
