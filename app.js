38e3c3742b41c1e37facb6d182a2b948ec6afb7d
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-05-06 18:07:58 +0300

    enable CORS and cors options

--- app.js
+++ app.js
@@ -18,21 +18,23 @@ const toursRouter = require('./routes/toursRoutes');
 const viewsRouter = require('./routes/viewsRoutes');
 const usersRouter = require('./routes/usersRoutes');
 const reviewsRouter = require('./routes/reviewsRoutes');
+const bookingController = require('./controllers/bookingController');
 //-------------------------------------------//
 const app = express();
+
+// Trust proxies
+app.enable('trust proxy');
 
 // Set the view engine to Pug
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-// Serve static content located in the "public" directory.
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
 //------------Global middleware--------------//
-// Compress all text sent to clients
-app.use(compression());
-
 // Enable CORS
 app.use(cors());
+app.options('*', cors());
+
+// Serve static content located in the "public" directory.
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(
@@ -49,14 +51,10 @@ app.use(
     }
   })
 );
-// Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
-
-// Data sanitization against XSS
-app.use(xss());
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Implement a simple rate limiting
 const limiter = rateLimit({
@@ -66,8 +64,22 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// Development logging
-app.use(morgan('dev'));
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Prevent parameter pollution
 app.use(
@@ -82,6 +94,9 @@ app.use(
     ]
   })
 );
+
+// Compress all text sent to clients
+app.use(compression());
 
 // Add a timestamp to each request
 // app.use((req, res, next) => {
