b76ab522b7d773c32e0a5c034f6b35acb9bc1c52
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-05-06 18:09:37 +0300

    handling closing server in case of SIGTERM signal from herouko

--- server.js
+++ server.js
@@ -37,3 +37,10 @@ process.on('unhandledRejection', err => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
